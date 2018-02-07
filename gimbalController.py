7794f8fe398551dec4664338c89e37bdae694eb3
Kenton Dover <kdover@localhost.localdomain>
2018-02-07 18:49:59 -0500

    created a setValue24 alternative that converts ints to shorts see wiki page https://github.com/ahearnkyle/GimbalProject/wiki/gimbal-connection

--- gimbalController.py
+++ gimbalController.py
@@ -1,3 +1,4 @@
+import struct
 
 '''
 
@@ -10,6 +11,7 @@ STX = 0x02 # Start of text sent by remote
 ETX = 0x03 # end of text sent by remote/PTR
 ACK = 0x06 # Acknowledge sent by PTR
 NAK = 0x15 # Not Acknowledge sent by PTR
+ESC = 0x1B # escape characters
 
 #stubbed out method
 def sendPacket(cmd, message):
@@ -27,19 +29,31 @@ def sendPacket(cmd, message):
             lrc ^= b
     print(lrc)
 
-#stubbed out method
-def setValue24(data, index, value):
-    data[index] = (value & 0xFF)
-    data[index + 1] = ((value & 0xFF00) >> 8)
-    data[index + 2] = ((value & 0xFF0000) >> 16)
-    return data
+
+def convertIntegerToShort(integer):
+    '''
+    This replaces the setvalue24 https://github.com/ahearnkyle/GimbalProject/wiki/gimbal-connection
+
+    This method will take an integer between 32768 and -32768 and will convert that
+    integer into a short int and return that as a byte string so 
+    32768 now equals b'\xff\x7f' with the \ signifying the different bits
+
+    This satisfies the following section of the protocol manual
+    
+    Passing Integer Values:
+    As noted in the protocol command descriptions, some values sent between the remote and PTR units are integer
+    values.  These integer values should be passed and received as 16-bit signed two's-complement little endian
+    integers simply split between two bytes.  The first byte should represent the LSB of the integer with the second byte
+    containing the MSB of the integer.  Negative values are represented as the two's-complement of the positive value 
+    '''
+    if integer > 32767 or integer < -32768:
+        return None
+    else:
+        return struct.pack("<h", integer) # < stands for little endian and h is for short
 
 def main():
     #sendPacket(0x36,None)
-    data = [0] * 6
-    data = setValue24(data, 0, 3*100)
-    data = setValue24(data, 3, 3*100)
-    print(data)
+    print(convertIntegerToShort(-2))
 
 if __name__ == '__main__':
     main()
