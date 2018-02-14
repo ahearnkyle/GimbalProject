e6b6d7aff6df5f717ea63a52d3934e3182e195f5
ahearnkyle <ahearnkyle01@gmail.com>
2018-02-14 00:41:00 +0000

    Updated some incorrect file path for web page python script

--- webApp/app.py
+++ webApp/app.py
@@ -1,11 +1,13 @@
 from flask import Flask,render_template
 
+#Creates a flask webpage
 app = Flask(__name__)
 
+#Creates a webpage based on the html template given
 @app.route('/')
 def index():
-	return render_template('index.html')
-
+	return render_template('main.js')
+#used to actually run the webpage server
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
 
