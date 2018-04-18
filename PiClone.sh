20dc1230887e169ae4d6c0af6917c8da648a7bc7
thugmunch <shell.21Wright.edu>
2018-04-18 18:01:05 -0400

    Updated the cloner script a while ago, just remembered to push it

--- PiClone.sh
+++ PiClone.sh
@@ -2,67 +2,70 @@
 
 #Author Jalen Shell.
 #This small script is to make a helpful way for a user to clone a system and restore it. Sudo permissions will be needed so it should be fine if you "sudo ./{This Program}".
-#Date Modified: 4/5/2018
+#Date Modified: 4/18/2018
 #Any questions can be emailed to jalenshell11@gmail.com about this script.
 #Website used to help create this script: https://wiki.archlinux.org/index.php/disk_cloning
 
+## Begin Functions.
 function runDD()
 {
-		echo "Enter path you want to copy (Source):"
-		read src
-		echo "Enter path you want paste (Destination):"
-		read dest
+		echo 
+		read -p "Enter path you want to copy (Source): " src
+		echo 
+		read -p "Enter path you want to paste (Destination): " dest
 		
-		echo "Is this a partion clone (y/n)?	(If no, then it will be a full disk clone) "
-		read decision
+		echo 
+		read -p "Is this a partition clone (y/n)?	(If no, then it will be a full disk clone) " decision
 		
-		if ($decision -eq "y"); 
+		if [ $decision = "y" ]; 
 		then
 			sudo dd if=$src of=$dest bs=64K conv=noerror,sync status=progress
 		else 
 			sudo dd if=$src of=$dest bs=64K conv=noerror,sync status=progress
 }
 
+# This function used to run the dd and fdisk and gzip programs to create an image and restore an image.
 function runDI()
 {
-	echo "Creating Image"
+	echo "Is this an image creation (y/n)?	(If no, then it will be a restore) "
+	read decision
+	
 	echo "Enter the path you want to get an image of: (Or the location of where to put the data back when restoring)"
 	read src
 	echo "Enter path you want to backup/save the image: (Or the backup location if restoring)"
 	read dest
 	
-	echo "Is this an image creation (y/n)?	(If no, then it will be a restore) "
-	read decision
-	
-	if ($decision -eq "y"); 
+	if [ $decision = "y" ]; 
 	then	
 		#Backup the drive
 		sudo dd if=$src conv=sync,noerror bs=64K | gzip -c > $dest.img.gz
 	
 		#More data about the drive is made
 		sudo fdisk -l $src > $dest/List__fdisk.info
-	
-	elif($decision -eq "n":
+		
+	elif [ $decision = "n" ]:
 	then	
 		gunzip -c $dest.img.gz | dd of=$src
 	
 }
+## End Functions.
 
-echo "Reminder: sudo needed."
+## MAIN
+echo "Reminder: sudo ./{This script} is needed."
 echo
-echo "What Cloner do you want to use?"
+echo "What Cloner/ do you want to use?"
 echo "1. dd"
 echo "2. partimage(More Visual Friendly)"
-echo "3. Create a disk image or Restore"
+echo "3. Create a disk image or Restore (Uses dd and fdisk)"
 
 #Get user input for this variable
-read choice
+read  -p "Choice: " choice
 
-if ($choice -eq 1); 
+if [ $choice = 1 ]; 
 then
 	echo "WARNING!!  Always ensure that the destination drive or partition is of equal or greater size than the source..."
 	# Check if dd is installed.
-	if command -v dd 2>/dev/null; 
+	if command -v dd >/dev/null; 
 	then
 		runDD
 	else
@@ -71,10 +74,10 @@ then
 		
 	fi
 	echo "Done!!"
-elif ($choice -eq 2); 
+elif [ $choice = 2 ]; 
 then
 	# Check if partimage is installed.
-	if command -v partimage 2>/dev/null; 
+	if command -v partimage >/dev/null; 
 	then
 		sudo partimage
 	else
@@ -82,18 +85,18 @@ then
 		sudo partimage
 	fi
 		echo "Done!!"
-elif ($choice -eq 3):
+elif [ $choice = 3 ]:
 then
-	# Check if dd and gzip is installed
-	if command -v dd 2>/dev/null; 
+	if command -v dd >/dev/null; 
 	then
 		sudo apt-get -y install dd
 	fi
-	if command -v gzip 2>/dev/null;
+	# Check if dd and gzip is installed
+	if command -v gzip >/dev/null;
 	then
 		sudo apt-get -y install gzip
 	fi
-	if command -v fdisk 2>/dev/null; 
+	if command -v fdisk >/dev/null; 
 	then
 		sudo apt-get -y install fdisk
 	fi
